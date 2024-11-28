c2522da468b7867ea1137f1d6dd2bbf044987a41
ashutoshORDS <ashnayak@icloud.com>
2024-11-28 19:30:15 +0530

    updated entity class

--- entities/Bin.java
+++ entities/Bin.java
@@ -1,5 +1,42 @@
 package entities;
 
-public class Bin {
+import java.util.ArrayList;
+import java.util.List;
 
+// based on how the person can move, decide width and depth
+public class Bin {
+    private String id;
+    private int height;
+    private int width;
+    private int depth;
+    private List<Item> items;
+
+    public Bin(String id, int height, int width, int depth) {
+        this.id = id;
+        this.height = height;
+        this.width = width;
+        this.depth = depth;
+        this.items = new ArrayList<>();
+    }
+
+    // Getters
+    public String getId() {
+        return id;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
 }
--- entities/Item.java
+++ entities/Item.java
@@ -1,5 +1,33 @@
 package entities;
 
 public class Item {
+    private String id;
+    private int height;
+    private int width;
+    private int depth;
+
+    public Item(String id, int height, int width, int depth) {
+        this.id = id;
+        this.height = height;
+        this.width = width;
+        this.depth = depth;
+    }
+
+    // Getters
+    public String getId() {
+        return id;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
 
 }
